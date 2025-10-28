6c91fe34b27c53f38151abe3339b1a6da92df25e
palomasantana-create <paloma.santana@userena.cl>
2025-10-27 21:08:45 -0300

    Add files via upload

---
+++ HorseallsolPalomaSantana.py
@@ -0,0 +1,47 @@
+tamaño = 5
+movimientos = [(2,1),(1,2),(-2,1),(-2,2),
+               (-2,-1),(-1,-2),(1,-2),(2,-1)]
+
+soluciones = []
+
+def es_valido(tablero, x,y):
+    return 0 <= x < tamaño and 0 <= y < tamaño and tablero[x] [y] == 0
+
+def encontrar_todas_soluciones(tablero, x,y, paso):
+    tablero[x] [y] = paso
+
+    if paso == tamaño * tamaño:
+        #Guardar una copia de la solucion
+        solucion = [fila[:] for fila in tablero]
+        soluciones.appened(solucion)
+        print(f"solución {len(soluciones)} encontrada!")
+    else: 
+        #Probar todas las soluciones
+        for dx, dy in movimientos:
+            nuevo_x, nuevo_y = x + dx, y + dy
+            if es_valido(tablero, nuevo_x, nuevo_y):
+                encontrar_todas_soluciones(tablero, nuevo_x, nuevo_y, paso + 1)
+
+    #Retroceder
+    tablero[x] [y] = 0
+
+def mostrar_solucion(solucion, número):
+    print(f"\n--- Solución {número} ---")
+    for fila in solucion:
+        for num in solucion:
+            print(f"{num: 3d}", end = " ")
+        print()
+
+#Programa principal
+tablero = [ [0 for _ in range(tamaño)] for _ in range(tamaño)]
+
+print("Buscando todas las soluciones...")
+encontrar_todas_soluciones(tablero, 0, 0, 1)
+
+print(f"\n Se encontraron {len(soluciones)} soluciones")
+
+#Mostrar algunas soluciones
+if soluciones:
+    for i in range(min(3, len(soluciones))):
+        mostrar_solucion(soluciones[i], i + 1)
+        
---
+++ HorseonesolPalomaSantana.py
@@ -0,0 +1,40 @@
+tamaño = 5
+movimientos = [(2,1),(1,2),(-1,2),(-2,1)
+               (-2,-1),(-1,-2),(2,-1),(1,-2)]
+
+soluciones = []
+
+def es_valido(tablero, x, y):
+    """Verificar si la posicion está dentro del tablero y libre"""
+    return 0 <= x < tamaño and 0 <= y < tamaño and tablero[x][y] == 0
+
+def resolver_caballo(tablero, x, y):
+    tablero [x] [y] = paso
+    if paso == tamaño * tamaño:
+        return True
+    
+    #Probar los 8 movimientos posibles
+    for dx, dy in movimientos:
+        nuevo_x, nuevo_y = x + dx, y + dy 
+
+        if es_valido(tablero, nuevo_x, nuevo_y):
+            if resolver_caballo(tablero, nuevo_x, nuevo_y, paso + 1):
+                return True
+            
+    tablero [x] [y] = 0 
+    return False
+
+def mostrar_tablero(tablero):
+    print(f"\n --- Solucion encontrada: ---")
+    for fila in tablero:
+        for num in fila: 
+            print(f"{num: 3d}", end = " ")
+        print()
+
+#programa principal
+tablero = [ [ 0 for _ in range(tamaño)] for _ in range(tamaño)]
+
+if resolver_caballo(tablero):
+    mostrar_tablero(tablero)
+else:
+    print("No se encontró solución.")
